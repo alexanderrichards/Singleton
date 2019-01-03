5d9ebdaceaf3d22d2ff8474488b831a231361000
Alexander Richards <a.richards@imperial.ac.uk>
2019-01-03 11:34:32 +0000

    Adding unit tests for underlying function and metaclass.

--- tests/test_singleton.py
+++ tests/test_singleton.py
@@ -1,6 +1,68 @@
 """Tests for the singleton module."""
 import unittest
+import mock
 from singleton._singleton import singleton, InstantiationError, SingletonMeta
+
+
+class TestSingletonMeta(unittest.TestCase):
+    """Unit tests for SingletonMeta metaclass."""
+
+    def setUp(self):
+        @singleton
+        class TestClass(object):
+            def __init__(self, a, b, c=90):
+                self.a, self.b, self.c = a, b, c
+        self.test_class = TestClass
+        self.assertIsInstance(self.test_class, SingletonMeta)
+        self.assertFalse(hasattr(self.test_class, '__instance__'))
+
+    def test_call(self):
+        with mock.patch.object(SingletonMeta, "__call__", side_effect=InstantiationError) as p:
+            with self.assertRaises(InstantiationError):
+                self.test_class(1, 2)
+            p.assert_called_with(1, 2)
+
+    def test_get_instance(self):
+        return_instance = self.test_class.__new__(self.test_class, 1, 2)
+        with mock.patch.object(SingletonMeta, "setup", return_value=return_instance) as p:
+            instance = self.test_class.get_instance(1, 2)
+            p.assert_called_with(1, 2)
+            self.assertIs(instance, return_instance)
+            setattr(self.test_class, "__instance__", return_instance)
+            other = self.test_class.get_instance()
+            p.assert_called_once_with(1, 2)
+        self.assertIs(other, instance)
+        self.assertIs(other, return_instance)
+
+    def test_setup(self):
+        return_instance = self.test_class.__new__(self.test_class, 1, 2)
+        with mock.patch.object(self.test_class, "__new__", return_value=return_instance) as p,\
+             mock.patch.object(self.test_class, "__init__") as q:
+            self.test_class.setup(1, 2)
+            p.assert_called_with(self.test_class, 1, 2)
+            q.assert_called_with(1, 2)
+
+        self.assertTrue(hasattr(self.test_class, '__instance__'))
+        self.assertIsNotNone(self.test_class.__instance__)
+        self.assertIsInstance(self.test_class.__instance__, self.test_class)
+        self.assertIs(self.test_class.__instance__, return_instance)
+        with self.assertRaises(InstantiationError):
+            self.test_class.setup(1, 2)
+
+
+class TestSingletonDecorator(unittest.TestCase):
+    """Unit tests for singleton decorator."""
+
+    def test_singleton(self):
+        with mock.patch.object(SingletonMeta, "__init__", return_value=None) as p:
+            @singleton
+            class TestClass(object):
+                def __init__(self, a, b, c=90):
+                    self.a, self.b, self.c = a, b, c
+            namespace = vars(TestClass).copy()
+            namespace.pop('__dict__', None)
+            namespace.pop('__weakref__', None)
+            p.assert_called_once_with(TestClass.__name__, TestClass.__bases__, namespace)
 
 
 class TestSingleton(unittest.TestCase):
